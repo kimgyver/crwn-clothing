591d0b683ed780f7c6b5fc3bdb33ddc82162317b
kimgyver <jinyoung777@gmail.com>
2019-07-27 19:45:57 +1200

    update menu-items to navigate withRouter HoC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
 import React from 'react';
-import {HomePage} from './pages/homepage.component'
+import HomePage from './pages/homepage.component'
+import {Route, Switch, Link} from 'react-router-dom'
+
 import './App.css';
 
+
+const HomePage2 = props => {
+  console.log(props);
+  return (
+    <div>
+      <button onClick={() => props.history.push('/blog/topics')}>Topics </button>
+      <h1>HOME PAGE</h1>
+    </div>
+  );
+};
+
+const HatsPage = () => (
+  <div>
+      <h1>Hats Page</h1>
+  </div>
+)
+
+const TopicsList = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>TOPIC LIST PAGE</h1>
+      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link><br/>
+      <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link><br/>
+      <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link><br/>
+    </div>
+  );
+};
+
+const TopicDetail = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage ></HomePage>
+       <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop/hats' component={HatsPage} />
+          <Route exact path='/blog/asdqw/topics' component={TopicsList} />
+          <Route path='/blog/aaaaa/topics/:topicId' component={TopicDetail} />
+          <Route exact path='/blog/topics' component={TopicsList} />
+          <Route path='/blog/topics/:topicId' component={TopicDetail} />
+       </Switch>
     </div>
   );
 }
--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Directory from '../components/directory/directory.component'
 import './homepage.styles.scss';    
 
-export const HomePage = () => (
+const HomePage = ({history}) => (
     <div className='homepage'>
-       <Directory/>
+       <Directory history={history}/>
     </div>
 )
+
+export default HomePage;
