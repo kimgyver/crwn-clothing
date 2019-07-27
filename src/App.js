e015f3ca0f715f4f16f266c6d2ce63c9c73aba8d
kimgyver <jinyoung777@gmail.com>
2019-07-27 20:56:36 +1200

    created collection preview class

--- src/App.js
+++ src/App.js
@@ -1,57 +1,17 @@
 import React from 'react';
 import HomePage from './pages/homepage.component'
 import {Route, Switch, Link} from 'react-router-dom'
+import ShopPage from './pages/shop/shop.component'
 
 import './App.css';
 
-
-const HomePage2 = props => {
-  console.log(props);
-  return (
-    <div>
-      <button onClick={() => props.history.push('/blog/topics')}>Topics </button>
-      <h1>HOME PAGE</h1>
-    </div>
-  );
-};
-
-const HatsPage = () => (
-  <div>
-      <h1>Hats Page</h1>
-  </div>
-)
-
-const TopicsList = props => {
-  console.log(props);
-  return (
-    <div>
-      <h1>TOPIC LIST PAGE</h1>
-      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link><br/>
-      <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link><br/>
-      <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link><br/>
-    </div>
-  );
-};
-
-const TopicDetail = props => {
-  console.log(props);
-  return (
-    <div>
-      <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div>
        <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/shop/hats' component={HatsPage} />
-          <Route exact path='/blog/asdqw/topics' component={TopicsList} />
-          <Route path='/blog/aaaaa/topics/:topicId' component={TopicDetail} />
-          <Route exact path='/blog/topics' component={TopicsList} />
-          <Route path='/blog/topics/:topicId' component={TopicDetail} />
+          <Route path='/shop' component={ShopPage} />
        </Switch>
     </div>
   );
