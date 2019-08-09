9a7554991c240841c70855ad2d0c7574e1823a3a
unknown <jinyoung777@gmail.com>
2019-08-09 18:28:47 +1200

    store collection and item data to firebase

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import ShopPage from './pages/shop/shop.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument, addCollectionAndDocuments} from './firebase/firebase.utils';
 import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/user/user.selectors';
 import {createStructuredSelector} from 'reselect';
+import {selectCollectionsForPreview} from './redux/shop/shop.selectors';
 
 import './App.css';
 
@@ -18,7 +19,7 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser, collectionsArry } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -33,6 +34,7 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth);
+      addCollectionAndDocuments('collections', collectionsArry.map(({title, items})=>({title, items})));
     });
   }
 
@@ -70,7 +72,8 @@ class App extends React.Component {
 //   currentUser: selectCurrentUser(state)
 // });
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  collectionsArry: selectCollectionsForPreview
 });
 
 
